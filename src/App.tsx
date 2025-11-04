a33669f0ee66a35381c642f15008dcdf8647b775
Letícia Gabrielly da Silva <165218856+LetGabs@users.noreply.github.com>
2025-11-04 00:24:31 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1bbdcdb8ae87d19871d9f2f76c5583d5385b5c25
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Reports from "./pages/Reports/Reports";
 import AuditPage from "./components/Página-Geral/AuditPage";
 import SettingsPage from "./components/Configurações/SettingsPage";
+import Colaboradores from "./pages/Colaboradores/Colaboradores";
 import "./style/estilo.css";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/relatorios" element={<Reports />} />
-      <Route path="/auditoria" element={<AuditPage />} />
-      <Route path="/configuracoes" element={<SettingsPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/colaboradores" element={<Colaboradores />} />
+        <Route path="/relatorios" element={<Reports />} />
+        <Route path="/auditoria" element={<AuditPage />} />
+        <Route path="/configuracoes" element={<SettingsPage />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
-<<<<<<< HEAD
-=======
->>>>>>> 5bc3cfb00f13230d406d1c57ebba92ecbf6c3d1d
-=======
->>>>>>> 1bbdcdb8ae87d19871d9f2f76c5583d5385b5c25
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1bbdcdb8ae87d19871d9f2f76c5583d5385b5c25
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './style/estilo.css';
-import './components/style.css'
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./style/estilo.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById("root");
 
-
-
-<<<<<<< HEAD
-=======
->>>>>>> 5bc3cfb00f13230d406d1c57ebba92ecbf6c3d1d
-=======
->>>>>>> 1bbdcdb8ae87d19871d9f2f76c5583d5385b5c25
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Elemento #root não encontrado no HTML!");
+}
